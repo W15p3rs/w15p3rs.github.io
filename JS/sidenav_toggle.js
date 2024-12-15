d94ee554b9e89c93716a1c8324518ec9703c6c14
W15p3rs <a.pdlyak@gmail.com>
2024-12-15 07:18:21 +0000

    added js for mobile responsive layout

--- JS/sidenav_toggle.js
+++ JS/sidenav_toggle.js
@@ -1,18 +1,27 @@
+
 function myFunction() {
-    var x = document.getElementById("mySidenav");
-    if (x.style.width === "200px") {
-      document.getElementById("mySidenav").style.width = "0px";
-      document.getElementById("main").style.marginRight= "0px";
-    } else {
-      document.getElementById("mySidenav").style.width = "200px";
-      document.getElementById("main").style.marginRight= "175px";
+    if(screen.width>=0){
+      var x = document.getElementById("mySidenav");
+      if (x.style.width === "200px") {
+        document.getElementById("mySidenav").style.width = "0px";
+        document.getElementById("main").style.marginRight= "0px";
+      } else {
+        document.getElementById("mySidenav").style.width = "200px";
+        document.getElementById("main").style.marginRight= "175px";
+      }
     }
-  } 
-  document.addEventListener("DOMContentLoaded", function () {
-    // Select the menu container
-    const menu = document.querySelector(".menu");
-  
-    // Add a click event listener to the menu
+    else{
+      if (x.style.width === "200px") {
+        document.getElementById("mySidenav").style.width = "0px";
+
+      } else {
+        document.getElementById("mySidenav").style.width = "200px";
+      }
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const menu = document.querySelector(".menu");
+     // Add a click event listener to the menu
     menu.addEventListener("click", function () {
       // Toggle the menuClick class on each line
       const lines = menu.querySelectorAll(".line");
